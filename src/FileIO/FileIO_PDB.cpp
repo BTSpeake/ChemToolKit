a1b062cb0adc89e79aa369a68a6bf306b384f566
Benjamin Speake <benjaminspeake@gmail.com>
2024-03-06 10:22:45 +0000

    Update PDB parser to read in bonding information if present

--- src/FileIO/FileIO_PDB.cpp
+++ src/FileIO/FileIO_PDB.cpp
@@ -21,6 +21,7 @@ void FileIO_PDB::read(ctkData::Molecule& mol) const {
     //std::regex headerRe(R"(^TITLE\s+(.*)$)");
     std::regex atomRe(R"(^ATOM\s+(\d+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\d+)\s+(\S?\d+\.\d+)\s+(\S?\d+\.\d+)\s+(\S?\d+\.\d+)\s+(\S?\d+\.\d+)\s+(\S?\d+\.\d+)\s+(\S+)\s+$)"); 
     std::regex hetAtomRe(R"(^HETATOM\s+(\d+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\d+)\s+(\S?\d+\.\d+)\s+(\S?\d+\.\d+)\s+(\S?\d+\.\d+)\s+(\S?\d+\.\d+)\s+(\S?\d+\.\d+)\s+(\S+)\s+$)");
+    std::regex conectRe(R"(^CONECT\s+(\d+))");
 
     while (getline(file, line)) {
         std::smatch match;
@@ -28,9 +29,23 @@ void FileIO_PDB::read(ctkData::Molecule& mol) const {
         // Parse an ATOM line 
         if (std::regex_match(line, match, atomRe)) {
             mol.addAtom(match[11], std::stod(match[6]), std::stod(match[7]), std::stod(match[8]));
+            mol.getAtom(mol.nAtoms() - 1)->addLabel("PDB_ATOM");
         }
         else if (std::regex_match(line, match, hetAtomRe)) {
             mol.addAtom(match[11], std::stod(match[6]), std::stod(match[7]), std::stod(match[8]));
+            mol.getAtom(mol.nAtoms() - 1)->addLabel("PDB_HETATOM");
+        }
+        else if (std::regex_search(line, match, conectRe)) {
+            int atmi = std::stoi(match[1]);
+            std::regex digitRe(R"((\d+))");
+            std::regex_token_iterator<std::string::iterator> rend;
+            std::regex_token_iterator<std::string::iterator> a(line.begin(), line.end(), digitRe);
+            while (a != rend) {
+                int atmj = std::stoi(*a++); 
+                if (atmj > atmi) {
+                    mol.addBond(atmi, atmj);
+                }
+            }
         }
         
     }
